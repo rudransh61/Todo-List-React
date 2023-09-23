f52880b733dee7ecbbd8b3d16d529ae4b29badd7
Rudransh <aarti19830@gmail.com>
2023-09-23 18:43:23 +0530

    init project todo in react only for practice improvements xx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+// import {List} from './List.js';
+import React, { useState } from 'react'
+
+
+function List (props) {
+  return (
+    <div>
+      <input type="checkbox"/>
+      <label for="vehicle1">
+         {props.task}
+      </label>
+    </div>
+  )
+}
+
 
 function App() {
+  const [list , change] = useState([]);
+  const add = () => {
+    let val1 = document.querySelector('#but').value;
+    console.log(val1);
+    change( list => [...list, val1]);
+    console.log(list);
+  }
+
+  const del = () => {
+    change(list.slice(0,-1));
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-slate-400">
+      <h>Todo List</h>
+      <button id="add" onClick={add}>Add+</button>
+      <button id="del" onClick={del}>Delete-</button>
+      <hr />
+      {/* <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/><label for="vehicle1"> I have a bike</label><br></br> */}
+      <input type="text" id="but" placeholder="Write here!" />
+      <div>
+
+        {console.log(list)}
+        {
+          list.map((i)=>{
+            return <List task={i}/>
+          })
+        }
+      </div>
     </div>
   );
 }
